bfcd77abfa617d9ae3949dafa8e1ddd1c70d7ca9
Dmitriy <pest2811@gmail.com>
2022-11-10 22:19:17 +0200

    add createAction & createReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit"
+
 export const fetchHeroes = (request) => (dispatch) => {
   dispatch(heroesFetching);
     request("http://localhost:3001/heroes")
@@ -13,64 +15,21 @@ export const fetchFilters = (request) => (dispatch) => {
       .catch(() => dispatch(filtersFetchingError()))
 }
 
+export const heroesFetching = createAction('HEROES_FETCHING');
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+export const heroesFetched = createAction('HEROES_FETCHED');
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
 
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+export const heroCreated = createAction('HERO_CREATED');
 
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload:hero
-    }
-}
-
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
-    }
-}
+export const heroDeleted = createAction('HERO_DELETED');
 
 
-export const filtersFetching = () => {
-    return {
-        type: 'FILTERS_FETCHING'
-    }
-}
+export const filtersFetching = createAction('FILTERS_FETCHING');
 
-export const filtersFetched = (filters) => {
-    return {
-        type: 'FILTERS_FETCHED',
-        payload: filters
-    }
-}
+export const filtersFetched = createAction('FILTERS_FETCHED');
 
-export const filtersFetchingError = () => {
-    return {
-        type: 'FILTERS_FETCHING_ERROR'
-    }
-}
+export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
 
-
-export const activeFilterChanged = (filter) => {
-    return {
-        type: 'ACTIVE_FITLER_CHANGED',
-        payload: filter
-    }
-}
+export const activeFilterChanged = createAction('ACTIVE_FITLER_CHANGED');
